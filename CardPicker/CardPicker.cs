e0f31ca3d954f940df561be5aa86ac5bba8e982b
mo4km <mo4km+it@protonmail.com>
2022-10-01 15:10:57 +0200

    added existing classes cardrPicker and enum CardSuite, changed namespaces

---
+++ CardPicker/CardPicker.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace CardPickerUI
+{
+    public class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            
+            string[] deck = new string[52];
+            //string[] numbers = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A" };
+            string[] drawnCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                drawnCards[i] = RandomValue() + " " + RandomSuit();
+            }
+
+            return drawnCards;
+        }
+
+        private static string RandomSuit()
+        {
+            CardSuit suit = (CardSuit)random.Next(1, 5);
+            return suit.ToString();            
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if(value == 1) { return "As"; }
+            if(value == 11) { return "Walet"; }
+            if(value == 12) { return "Dama"; }
+            if(value == 13) { return "Krol"; }
+            return value.ToString();
+        }
+    }
+}
---
+++ CardPicker/CardSuit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardPickerUI
+{
+    public enum CardSuit
+    {
+        Kier = 1,
+        Trefl,
+        Pik,
+        Karo
+
+    }
+}
